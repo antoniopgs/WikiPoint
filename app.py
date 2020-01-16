d22ab56e3834962c4351b8db4be28795c4ac0f4a
António Gonçalves <44982443+antoniopgs@users.noreply.github.com>
2020-01-16 21:31:03 +0000

    Simpler Scrape by #Ref
    Instead of manually iterating over the entire html, this is a possibly simpler method, where I just make new requests, using the sub_pages's references.

--- app.py
+++ app.py
@@ -2,43 +2,51 @@ import requests
 from bs4 import BeautifulSoup
 
 
-search_term = input("Search: ").title()
-url = "https://www.britannica.com/place/" + search_term
+def read(x):
+    info = x.find_all("p")
+    text = ""
+    for p in info:
+        text_piece = p.get_text()
+        text += text_piece
+    text = text.strip("Our editors will review what you’ve submitted and determine whether to revise the article.")
+    return text
 
+def write(x):
+    data = read(x)
+    with open("data.txt", "w") as file:
+        file.write(data)
+
+
+# Receive Input:
+search_term = input("Search: ").title()
+# Build URL:
+url = "https://www.britannica.com/place/" + search_term
+# Get Request:
 request = requests.get(url)
+
+# If Request is Successful
 if request.status_code == 200:
     html = request.text
     soup = BeautifulSoup(html, 'html.parser')
-    sub_page_list = soup.find("ul", {"data-level" : "h1"})
 
-    # Try to Find Children/Sub-Topics.
-    try:
-        # The False recursive is to only search for direct children.
-        sub_info = sub_page_list.findAll("li", recursive=False)
-        sub_info_2 = [child.find(href=True) for child in sub_info]
-        
-        # Extracting Links.
-        links = [x["href"] for x in sub_info_2]
-        # Extracting Topics.
-        topics = [x.get_text().title() for x in sub_info_2]
-        # Merging Links and Topics Together.
-        links_and_topics = list(zip(links, topics))
-        
-        for i in links_and_topics:
-            print(i)
-            print()
-        for i in links_and_topics:
-            print(i)
-            print()
+    # Check for Sub Sections:
+    sub_sections = soup.find("ul", {"data-level" : "h1"})
 
-    # If there are no Sub-Topics, just print the text.        
-    except AttributeError:
-        info = soup.find_all("p")
-        text = ""
-        for p in info:
-            text_piece = p.get_text()
-            text += text_piece
-        text = text.strip("Our editors will review what you’ve submitted and determine whether to revise the article.")
-        print(text)
+    # If there are sub sections:
+    if sub_sections:
+        # I can add the data target from any subsection (except 1st) to the url and get full article access:
+        sub_sections_2 = sub_sections.findAll("li")
+        url_extension = sub_sections_2[1].get("data-target")
+        url += url_extension
+        url = "https://www.britannica.com/place/Lisbon/The-20th-century"
+        request = requests.get(url)
+        html = request.text
+        soup = BeautifulSoup(html, 'html.parser')
+        write(soup)
+    else:
+    # If there are no Sub Sections, just print the text regularly:      
+        write(soup)
+
+# If Page Is Not Found:
 elif request.status_code == 404:
     print("No article was found.")
