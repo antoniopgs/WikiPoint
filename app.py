62948ce4d987c44208a901828b0b3fe227a9d26e
António Gonçalves <44982443+antoniopgs@users.noreply.github.com>
2020-01-17 18:30:34 +0000

    Switched to Wikipedia
    To Do:
     - Implement Dict/JSON instead of Str
     - Remove Footnotes

--- app.py
+++ app.py
@@ -1,44 +1,62 @@
-import requests
+from pptx import Presentation
 from bs4 import BeautifulSoup
+import requests
+import json
+import re
 
 
+# Get Inputs
 search_term = input("Search: ").title()
-url = "https://www.britannica.com/place/" + search_term
 
+# Build URL
+url = "https://en.wikipedia.org/wiki/" + search_term
+
+# Make Request
 request = requests.get(url)
+
+# Evaluate Status Code
 if request.status_code == 200:
-    html = request.text
-    soup = BeautifulSoup(html, 'html.parser')
-    sub_page_list = soup.find("ul", {"data-level" : "h1"})
+    
+    # Get Data
+    soup = BeautifulSoup(request.text, "html.parser")
 
-    # Try to Find Children/Sub-Topics.
-    try:
-        # The False recursive is to only search for direct children.
-        sub_info = sub_page_list.findAll("li", recursive=False)
-        sub_info_2 = [child.find(href=True) for child in sub_info]
-        
-        # Extracting Links.
-        links = [x["href"] for x in sub_info_2]
-        # Extracting Topics.
-        topics = [x.get_text().title() for x in sub_info_2]
-        # Merging Links and Topics Together.
-        links_and_topics = list(zip(links, topics))
-        
-        for i in links_and_topics:
-            print(i)
-            print()
-        for i in links_and_topics:
-            print(i)
-            print()
+    # Remove All Tables
+    soup.table.decompose()
 
-    # If there are no Sub-Topics, just print the text.        
-    except AttributeError:
-        info = soup.find_all("p")
-        text = ""
-        for p in info:
-            text_piece = p.get_text()
-            text += text_piece
-        text = text.strip("Our editors will review what you’ve submitted and determine whether to revise the article.")
-        print(text)
+    # Get Headers and Paragraphs
+    paragraphs = soup.findAll(["h1", "h2", "h3", "h4", "h5", "h6", "p"])
+
+    """# Get JSON/Dict
+    data = {}
+    for paragraph in paragraphs:
+        data += json.loads(paragraph)"""
+    
+    # Extract Text Only
+    clean_paragraphs = [paragraph.get_text() for paragraph in paragraphs]
+
+    # Join Pieces of Text Together
+    text = "\n".join(clean_paragraphs)
+    
+    # Using Regex to Remove "[x]"
+    regex = re.compile("\[.*\]")
+    clean_text = regex.sub("", text)
+
+    # Write Text to File:
+    with open("text.txt", "w", encoding = "utf-8") as file:
+        file.write(clean_text)
+
+    # Create PowerPoint Presentation
+    ppt = Presentation()
+    title_slide_layout = ppt.slide_layouts[0]
+    title_slide = ppt.slides.add_slide(title_slide_layout)
+    title = title_slide.shapes.title
+    title.text = search_term
+    ppt.save('presentation.pptx')
+        
 elif request.status_code == 404:
-    print("No article was found.")
+    print("No data was found.")
+else:
+    print("An error occurred.")
+
+
+
