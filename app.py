2be4ccee8d34a2bfd216a12bb0c1cf9502055388
António Gonçalves <44982443+antoniopgs@users.noreply.github.com>
2020-01-21 18:35:07 +0000

    Implement "split_sections" function

--- app.py
+++ app.py
@@ -28,46 +28,45 @@ def select_topic(search_term):
         except TypeError:
             print("Invalid Topic Number.\n")
 
+def split_sections(data):
+    # Detect Tags:
+    h2_tags = [h2.strip("\n") for h2 in re.findall("\n== .* ==\n", data)]
+    h3_tags = [h3.strip("\n") for h3 in re.findall("\n=== .* ===\n", data)]
+    h4_tags = [h4.strip("\n") for h4 in re.findall("\n==== .* ===\n", data)]
+    h5_tags = [h5.strip("\n") for h5 in re.findall("\n===== .* =====\n", data)]
+    h6_tags = [h6.strip("\n") for h6 in re.findall("\n====== .* ======\n", data)]
+    # Create Tags Group:
+    tags_group = [h2_tags, h3_tags, h4_tags, h5_tags, h6_tags]
+    # Create Sections Group:
+    h2_sections, h3_sections, h4_sections, h5_sections, h6_sections = [], [], [], [], []
+    sections_group = [h2_sections, h3_sections, h4_sections, h5_sections, h6_sections]
+    # Generate:
+    for tags, sections in zip(tags_group, sections_group):
+        for i in range(len(tags)):
+            current_tag = tags[i]
+            try:
+                next_tag = tags[i+1]
+                section = data[data.index(current_tag):data.index(next_tag)].strip(current_tag).strip()
+            except IndexError:
+                section = data[data.index(current_tag):].strip(current_tag).strip()
+            sections.append([current_tag, section])
+    return sections_group
+    
+
 search_result = search()
 topic_result = select_topic(search_result)
 page = wikipedia.page(search_result[topic_result-1])
 pre_info = page.content.split("\n== See also ==\n")
 info = pre_info[0].strip()
+organized_info = split_sections(info)
+
+for generated_list in organized_info:
+    for element in generated_list:
+        print(f"\n----- Printing {element[0]} -----\n")
+        print(element[1])
 
 with open("text.txt", "w", encoding="utf-8") as file:
     file.write(info)
-
-h2_tags = [h2.strip("\n") for h2 in re.findall("\n== .* ==\n", info)]
-h3_tags = [h3.strip("\n") for h3 in re.findall("\n=== .* ===\n", info)]
-h4_tags = [h4.strip("\n") for h4 in re.findall("\n==== .* ===\n", info)]
-h5_tags = [h5.strip("\n") for h5 in re.findall("\n===== .* =====\n", info)]
-h6_tags = [h6.strip("\n") for h6 in re.findall("\n====== .* ======\n", info)]
-
-# Break Into H2 Sections:
-h2_sections = {}
-for i in range(len(h2_tags)):
-    current_tag = h2_tags[i]
-    try:
-        next_tag = h2_tags[i+1]
-        h2_section = info[info.index(current_tag):info.index(next_tag)].strip(current_tag).strip()
-    except IndexError:
-        h2_section = info[info.index(current_tag):].strip(current_tag).strip()
-    h2_sections[current_tag] = h2_section 
-
-h3_sections = {}
-for value in h2_sections.values():
-    for i in range(len(h3_tags)):
-        current_tag = h3_tags[i]
-        try:
-            next_tag = h3_tags[i+1]
-            h3_section = value[value.index(current_tag):value.index(next_tag)].strip(current_tag).strip()
-        except IndexError:
-            h3_section = value[value.index(current_tag):].strip(current_tag).strip()
-        h3_sections[current_tag] = h3_section
-
-for key, value in h2_sections.items():
-    print(f"Key: {key}, Value: {value}")
-    print("----- BREAK -----")
 
 # Create PowerPoint Presentation
 ppt = Presentation()
