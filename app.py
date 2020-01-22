2cd580788139e33c8c7eb5013b1be32cbcaec04a
António Gonçalves <44982443+antoniopgs@users.noreply.github.com>
2020-01-22 18:15:36 +0000

    Add PPT Index Generator

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ with open("text.txt", "w", encoding="utf-8") as file:
     file.write(info)
 
 # Find all tags and add page title as first tag:
-tags = [tag.replace("=", "").strip().title() for tag in re.findall("\n==.*==\n", info)]
+tags = [tag.strip().title() for tag in re.findall("\n==.*==\n", info)]
 tags.insert(0, page.title)
 
 # Split by all tags. Get tag's texts:
@@ -48,14 +48,10 @@ tags_text = [section.strip() for section in re.split("\n==.*==\n", info)]
 
 # Create List with all Tags and Text:
 sections = list(zip(tags, tags_text))
-for section in sections:
-    print(f"Tag: {section[0]}")
-    print()
-    print(section[1])
-    print("\n ---- BREAK -----\n")
 
-# PowerPoint Generator:
+# Create PowerPoint:
 ppt = Presentation()
+# Slide Templates:
 just_title = ppt.slide_layouts[0]
 title_content = ppt.slide_layouts[1]
 section_header = ppt.slide_layouts[2]
@@ -65,11 +61,36 @@ title_only = ppt.slide_layouts[5]
 blank = ppt.slide_layouts[6]
 content_caption = ppt.slide_layouts[7]
 picture_caption = ppt.slide_layouts[8]
+# Generator:
 for i in range(len(sections)):
     if i == 0:
-        slide = ppt.slides.add_slide(just_title)
+        # Cover Slide:
+        cover_slide = ppt.slides.add_slide(just_title)
+        cover_slide_title = cover_slide.shapes.title
+        cover_slide_title.text = sections[0][0]
+        # Index Slide:
+        index_slide = ppt.slides.add_slide(title_content)
+        index_slide_title = index_slide.shapes.title
+        index_slide_title.text = "Index"
+        # Index Generator:
+        index = index_slide.shapes.placeholders[1]
+        tf = index.text_frame
+        #tf.text = "Find the bullet slide layout"
+        for section in sections[1:]:
+            p = tf.add_paragraph()
+            if re.match("={2} .* ={2}", section[0]):
+                p.level = 1
+            elif re.match("={3} .* ={3}", section[0]):
+                p.level = 2
+            elif re.match("={4} .* ={4}", section[0]):
+                p.level = 3
+            elif re.match("={5} .* ={5}", section[0]):
+                p.level = 4
+            elif re.match("={6} .* ={6}", section[0]):
+                p.level = 5
+            p.text = section[0].replace("=", "").strip()
     else:
-        slide = ppt.slides.add_slide(section_header)
-    title = slide.shapes.title
-    title.text = sections[i][0]
+        info_slide = ppt.slides.add_slide(section_header)
+        title = info_slide.shapes.title
+        title.text = sections[i][0]
 ppt.save('presentation.pptx')
